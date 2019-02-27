50488390ddd55a4c8c59b4f1110729c03bb92573
Matt Burgess <matthewburgess@digital.uc.dwp.gov.uk>
2019-02-27 10:52:04 +0000

    Add initial feature set

---
+++ vpc.tf
@@ -0,0 +1,68 @@
+resource "aws_vpc" "vpc" {
+  cidr_block           = "${var.vpc_cidr_block}"
+  instance_tenancy     = "${var.vpc_instance_tenancy}"
+  enable_dns_hostnames = "${var.vpc_enable_dns_hostnames}"
+
+  tags = "${merge(
+    "${var.common_tags}",
+    map("Name", "${var.vpc_name}")
+  )}"
+}
+
+resource "aws_default_security_group" "default" {
+  vpc_id = "${aws_vpc.vpc.id}"
+}
+
+resource "aws_flow_log" "flow_log" {
+  iam_role_arn    = "${aws_iam_role.vpc_flow_logs.arn}"
+  log_destination = "${aws_cloudwatch_log_group.vpc_flow_logs.arn}"
+  traffic_type    = "${var.vpc_flow_log_traffic_type}"
+  vpc_id          = "${aws_vpc.vpc.id}"
+}
+
+resource "aws_cloudwatch_log_group" "vpc_flow_logs" {
+  name              = "/aws/vpc-flow-logs/${var.vpc_name}"
+  retention_in_days = "${var.vpc_flow_log_retention_days}"
+  tags              = "${var.common_tags}"
+}
+
+data "aws_iam_policy_document" "vpc_flow_logs_assume_role" {
+  statement {
+    sid    = "VpcFlowLogsAssumeRole"
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["vpc-flow-logs.amazonaws.com"]
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+data "aws_iam_policy_document" "vpc_flow_logs" {
+  statement {
+    sid       = "VpcFlowLogs_${var.vpc_name}"
+    effect    = "Allow"
+    resources = ["${aws_cloudwatch_log_group.vpc_flow_logs.arn}"]
+
+    actions = [
+      "logs:CreateLogGroup",
+      "logs:CreateLogStream",
+      "logs:PutLogEvents",
+      "logs:DescribeLogGroups",
+      "logs:DescribeLogStreams",
+    ]
+  }
+}
+
+resource "aws_iam_role" "vpc_flow_logs" {
+  name               = "vpc_flow_logs_${var.vpc_name}"
+  assume_role_policy = "${data.aws_iam_policy_document.vpc_flow_logs_assume_role.json}"
+}
+
+resource "aws_iam_role_policy" "vpc_flow_logs" {
+  name   = "vpc_flow_logs_${var.vpc_name}"
+  role   = "${aws_iam_role.vpc_flow_logs.id}"
+  policy = "${data.aws_iam_policy_document.vpc_flow_logs.json}"
+}
---
+++ vpc_endpoints.tf
@@ -0,0 +1,96 @@
+resource "aws_vpc_endpoint" "s3" {
+  count             = "${var.s3_endpoint ? 1: 0}"
+  service_name      = "com.amazonaws.${var.region}.s3"
+  vpc_id            = "${aws_vpc.vpc.id}"
+  vpc_endpoint_type = "Gateway"
+  route_table_ids   = ["${var.gateway_vpce_route_table_ids}"]
+}
+
+resource "aws_vpc_endpoint" "ssm" {
+  count               = "${var.ssm_endpoint ? 1 : 0}"
+  service_name        = "com.amazonaws.${var.region}.ssm"
+  vpc_id              = "${aws_vpc.vpc.id}"
+  vpc_endpoint_type   = "Interface"
+  security_group_ids  = ["${aws_security_group.vpc_endpoints.id}"]
+  subnet_ids          = ["${var.interface_vpce_subnet_ids}"]
+  private_dns_enabled = true
+}
+
+resource "aws_vpc_endpoint" "ec2messages" {
+  count               = "${var.ec2messages_endpoint ? 1 : 0}"
+  service_name        = "com.amazonaws.${var.region}.ec2messages"
+  vpc_id              = "${aws_vpc.vpc.id}"
+  vpc_endpoint_type   = "Interface"
+  security_group_ids  = ["${aws_security_group.vpc_endpoints.id}"]
+  subnet_ids          = ["${var.interface_vpce_subnet_ids}"]
+  private_dns_enabled = true
+}
+
+resource "aws_vpc_endpoint" "ec2" {
+  count               = "${var.ec2_endpoint ? 1 : 0}"
+  service_name        = "com.amazonaws.${var.region}.ec2"
+  vpc_id              = "${aws_vpc.vpc.id}"
+  vpc_endpoint_type   = "Interface"
+  security_group_ids  = ["${aws_security_group.vpc_endpoints.id}"]
+  subnet_ids          = ["${var.interface_vpce_subnet_ids}"]
+  private_dns_enabled = true
+}
+
+resource "aws_vpc_endpoint" "ssmmessages" {
+  count               = "${var.ssmmessages_endpoint ? 1 : 0}"
+  service_name        = "com.amazonaws.${var.region}.ssmmessages"
+  vpc_id              = "${aws_vpc.vpc.id}"
+  vpc_endpoint_type   = "Interface"
+  security_group_ids  = ["${aws_security_group.vpc_endpoints.id}"]
+  subnet_ids          = ["${var.interface_vpce_subnet_ids}"]
+  private_dns_enabled = true
+}
+
+resource "aws_vpc_endpoint" "logs" {
+  count               = "${var.logs_endpoint ? 1 : 0}"
+  service_name        = "com.amazonaws.${var.region}.logs"
+  vpc_id              = "${aws_vpc.vpc.id}"
+  vpc_endpoint_type   = "Interface"
+  security_group_ids  = ["${aws_security_group.vpc_endpoints.id}"]
+  subnet_ids          = ["${var.interface_vpce_subnet_ids}"]
+  private_dns_enabled = true
+}
+
+resource "aws_vpc_endpoint" "sns" {
+  count               = "${var.sns_endpoint ? 1 : 0}"
+  service_name        = "com.amazonaws.${var.region}.sns"
+  vpc_id              = "${aws_vpc.vpc.id}"
+  vpc_endpoint_type   = "Interface"
+  security_group_ids  = ["${aws_security_group.vpc_endpoints.id}"]
+  subnet_ids          = ["${var.interface_vpce_subnet_ids}"]
+  private_dns_enabled = true
+}
+
+resource "aws_security_group" "vpc_endpoints" {
+  name        = "vpc-endpoints-${var.vpc_name}"
+  description = "Allows instances to reach Interface VPC endpoints"
+  vpc_id      = "${aws_vpc.vpc.id}"
+  tags        = "${var.common_tags}"
+}
+
+resource "aws_security_group_rule" "vpce_kafka_https_ingress" {
+  count                    = "${var.interface_vpce_source_security_group_count}"
+  description              = "Accept VPCE traffic"
+  type                     = "ingress"
+  protocol                 = "tcp"
+  from_port                = 443
+  to_port                  = 443
+  source_security_group_id = "${element(var.interface_vpce_source_security_group_ids, count.index)}"
+  security_group_id        = "${aws_security_group.vpc_endpoints.id}"
+}
+
+resource "aws_security_group_rule" "kafka_vpce_https_egress" {
+  count                    = "${var.interface_vpce_source_security_group_count}"
+  description              = "Allow outbound requests to VPC endpoints"
+  type                     = "egress"
+  protocol                 = "tcp"
+  from_port                = 443
+  to_port                  = 443
+  source_security_group_id = "${element(var.interface_vpce_source_security_group_ids, count.index)}"
+  security_group_id        = "${aws_security_group.vpc_endpoints.id}"
+}
