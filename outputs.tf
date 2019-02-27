ad3a77ff280a99eb9583c285937f7cf91d1cda64
Matthew Burgess <549318+mattburgess@users.noreply.github.com>
2019-02-27 16:04:35 +0000

    Add initial feature set (#1)
    * Add initial feature set
    
    * Add CI job and GitHub repo config
    
    * Simplify variable descriptions
    
    The required/optional status of a variable and its default status are
    documented automatically by Terraform Registry
    
    * Make SG rule names generic

---
+++ outputs.tf
@@ -0,0 +1,19 @@
+output "vpc_id" {
+  value = "${aws_vpc.vpc.id}"
+}
+
+output "vpc_cidr_block" {
+  value = "${aws_vpc.vpc.cidr_block}"
+}
+
+output "interface_vpce_sg_id" {
+  value = "${aws_security_group.vpc_endpoints.id}"
+}
+
+output "ssm_iam_role_name" {
+  value = "${aws_iam_role.ssm.name}"
+}
+
+output "ssm_instance_profile_name" {
+  value = "${aws_iam_instance_profile.ssm.name}"
+}
---
+++ ssm_role.tf
@@ -0,0 +1,27 @@
+resource "aws_iam_role" "ssm" {
+  name               = "ssm_${var.vpc_name}"
+  assume_role_policy = "${data.aws_iam_policy_document.ec2_assume_role.json}"
+}
+
+resource "aws_iam_role_policy_attachment" "ec2_for_ssm_attachment" {
+  role       = "${aws_iam_role.ssm.name}"
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2RoleforSSM"
+}
+
+data "aws_iam_policy_document" "ec2_assume_role" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["ec2.amazonaws.com"]
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+resource "aws_iam_instance_profile" "ssm" {
+  name = "ssm_${var.vpc_name}"
+  role = "${aws_iam_role.ssm.name}"
+}
