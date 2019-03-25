04f839fb876d8b0644c0c373694cc23c9b746d43
Matt Burgess <matthewburgess@digital.uc.dwp.gov.uk>
2019-03-25 11:17:33 +0000

    Output the VPC's main route table ID
    Consumers of this module may wish to add routing rules to the main route table.

--- outputs.tf
+++ outputs.tf
@@ -6,6 +6,10 @@ output "vpc_cidr_block" {
   value = "${aws_vpc.vpc.cidr_block}"
 }
 
+output "main_route_table_id" {
+  value = "${aws_vpc.vpc.vpc_main_route_table_id}"
+}
+
 output "interface_vpce_sg_id" {
   value = "${aws_security_group.vpc_endpoints.id}"
 }
