f50ec90a59243f2df0f32da55249de43761d9310
Matthew Burgess <549318+mattburgess@users.noreply.github.com>
2019-03-29 11:23:04 +0000

    Workaround Terraform's lack of short-circuit evaluation (#11)

--- outputs.tf
+++ outputs.tf
@@ -14,18 +14,24 @@ output "interface_vpce_sg_id" {
   value = "${aws_security_group.vpc_endpoints.id}"
 }
 
+# Note that all of the outputs below are conditional, based on their respective
+# endpoints having been requested. Naively, you'd think the following would work:
+# value = "${var.s3_endpoint ? aws_vpc_endpoint.s3.0.prefix_list_id : ""}"
+# but you end up running into https://github.com/hashicorp/hil/issues/50 so we
+# use the workaround from that issue to achieve the same goal
+
 output "s3_prefix_list_id" {
-  value = "${var.s3_endpoint ? aws_vpc_endpoint.s3.0.prefix_list_id: ""}"
+  value = "${element(compact(concat(list(var.s3_endpoint), aws_vpc_endpoint.s3.*.prefix_list_id)), 0)}"
 }
 
 output "dynamodb_prefix_list_id" {
-  value = "${var.dynamodb_endpoint ? aws_vpc_endpoint.dynamodb.0.prefix_list_id: ""}"
+  value = "${element(compact(concat(list(var.dynamodb_endpoint), aws_vpc_endpoint.dynamodb.*.prefix_list_id)), 0)}"
 }
 
 output "ssm_iam_role_name" {
-  value = "${aws_iam_role.ssm.0.name}"
+  value = "${element(compact(concat(list(var.ssm_endpoint), aws_iam_role.ssm.*.name)), 0)}"
 }
 
 output "ssm_instance_profile_name" {
-  value = "${aws_iam_instance_profile.ssm.0.name}"
+  value = "${element(compact(concat(list(var.ssm_endpoint), aws_iam_instance_profile.ssm.*.name)), 0)}"
 }
