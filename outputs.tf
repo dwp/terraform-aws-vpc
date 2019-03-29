ac9839fd293757f85fa198e3bf8232a63421d78f
Matthew Burgess <549318+mattburgess@users.noreply.github.com>
2019-03-29 09:36:36 +0000

    Output gateway VPC endpoint prefix list IDs (#10)
    * Output gateway VPC endpoint prefix list IDs
    
    These may need to be referenced by a caller's security group rules
    
    * Output string rather than list for SSM IAM outputs
    
    This means that callers don't have to index into the list to obtain the
    single output they need to use.

--- outputs.tf
+++ outputs.tf
@@ -14,10 +14,18 @@ output "interface_vpce_sg_id" {
   value = "${aws_security_group.vpc_endpoints.id}"
 }
 
+output "s3_prefix_list_id" {
+  value = "${var.s3_endpoint ? aws_vpc_endpoint.s3.0.prefix_list_id: ""}"
+}
+
+output "dynamodb_prefix_list_id" {
+  value = "${var.dynamodb_endpoint ? aws_vpc_endpoint.dynamodb.0.prefix_list_id: ""}"
+}
+
 output "ssm_iam_role_name" {
-  value = "${aws_iam_role.ssm.*.name}"
+  value = "${aws_iam_role.ssm.0.name}"
 }
 
 output "ssm_instance_profile_name" {
-  value = "${aws_iam_instance_profile.ssm.*.name}"
+  value = "${aws_iam_instance_profile.ssm.0.name}"
 }
