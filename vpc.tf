1d5a2635a35bdffd43d0174efbd8c9b69c200c0f
Daniel Hill <dan@mamu.co>
2019-11-19 16:39:02 +0000

    fix Terraform 0.12 issues (#18)
    * fix Terraform 0.12 issues in SSM role
    * change all interpolation to match Terraform 0.12 standards
    
    Signed-off-by: Daniel Hill <danhill@digital.uc.dwp.gov.uk>

--- vpc.tf
+++ vpc.tf
@@ -1,29 +1,29 @@
 resource "aws_vpc" "vpc" {
-  cidr_block           = "${var.vpc_cidr_block}"
-  instance_tenancy     = "${var.vpc_instance_tenancy}"
-  enable_dns_hostnames = "${var.vpc_enable_dns_hostnames}"
+  cidr_block           = var.vpc_cidr_block
+  instance_tenancy     = var.vpc_instance_tenancy
+  enable_dns_hostnames = var.vpc_enable_dns_hostnames
 
-  tags = "${merge(
-    "${var.common_tags}",
-    map("Name", "${var.vpc_name}")
-  )}"
+  tags = merge(
+    var.common_tags,
+    map("Name", var.vpc_name)
+  )
 }
 
 resource "aws_default_security_group" "default" {
-  vpc_id = "${aws_vpc.vpc.id}"
+  vpc_id = aws_vpc.vpc.id
 }
 
 resource "aws_flow_log" "flow_log" {
-  iam_role_arn    = "${aws_iam_role.vpc_flow_logs.arn}"
-  log_destination = "${aws_cloudwatch_log_group.vpc_flow_logs.arn}"
-  traffic_type    = "${var.vpc_flow_log_traffic_type}"
-  vpc_id          = "${aws_vpc.vpc.id}"
+  iam_role_arn    = aws_iam_role.vpc_flow_logs.arn
+  log_destination = aws_cloudwatch_log_group.vpc_flow_logs.arn
+  traffic_type    = var.vpc_flow_log_traffic_type
+  vpc_id          = aws_vpc.vpc.id
 }
 
 resource "aws_cloudwatch_log_group" "vpc_flow_logs" {
   name              = "/aws/vpc-flow-logs/${var.vpc_name}"
-  retention_in_days = "${var.vpc_flow_log_retention_days}"
-  tags              = "${var.common_tags}"
+  retention_in_days = var.vpc_flow_log_retention_days
+  tags              = var.common_tags
 }
 
 data "aws_iam_policy_document" "vpc_flow_logs_assume_role" {
@@ -44,7 +44,7 @@ data "aws_iam_policy_document" "vpc_flow_logs" {
   statement {
     sid       = "VpcFlowLogs${replace(var.vpc_name, "-", "")}"
     effect    = "Allow"
-    resources = ["${aws_cloudwatch_log_group.vpc_flow_logs.arn}"]
+    resources = [aws_cloudwatch_log_group.vpc_flow_logs.arn]
 
     actions = [
       "logs:CreateLogGroup",
@@ -58,11 +58,11 @@ data "aws_iam_policy_document" "vpc_flow_logs" {
 
 resource "aws_iam_role" "vpc_flow_logs" {
   name               = "vpc_flow_logs_${var.vpc_name}"
-  assume_role_policy = "${data.aws_iam_policy_document.vpc_flow_logs_assume_role.json}"
+  assume_role_policy = data.aws_iam_policy_document.vpc_flow_logs_assume_role.json
 }
 
 resource "aws_iam_role_policy" "vpc_flow_logs" {
   name   = "vpc_flow_logs_${var.vpc_name}"
-  role   = "${aws_iam_role.vpc_flow_logs.id}"
-  policy = "${data.aws_iam_policy_document.vpc_flow_logs.json}"
+  role   = aws_iam_role.vpc_flow_logs.id
+  policy = data.aws_iam_policy_document.vpc_flow_logs.json
 }
