496208c08cac7d93930513fb92c32d4fc5674e35
Daniel Hill <danhill@digital.uc.dwp.gov.uk>
2020-02-07 13:55:49 +0000

    BREAKING CHANGE: remove `aws_vpc.vpc.*` outputs
    Signed-off-by: Daniel Hill <danhill@digital.uc.dwp.gov.uk>

--- outputs.tf
+++ outputs.tf
@@ -2,18 +2,6 @@ output "vpc" {
   value = aws_vpc.vpc
 }
 
-output "vpc_id" {
-  value = aws_vpc.vpc.id
-}
-
-output "vpc_cidr_block" {
-  value = aws_vpc.vpc.cidr_block
-}
-
-output "main_route_table_id" {
-  value = aws_vpc.vpc.main_route_table_id
-}
-
 output "interface_vpce_sg_id" {
   value = aws_security_group.vpc_endpoints.id
 }
