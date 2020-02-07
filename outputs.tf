78b12c463d37623232792df738e99275e9793d9c
Daniel Hill <dan@mamu.co>
2020-02-07 19:13:02 +0000

    BREAKING CHANGE: output the full VPC object (#22)
    and remove `aws_vpc.vpc.*` outputs
    Signed-off-by: Daniel Hill <danhill@digital.uc.dwp.gov.uk>

--- outputs.tf
+++ outputs.tf
@@ -1,13 +1,5 @@
-output "vpc_id" {
-  value = aws_vpc.vpc.id
-}
-
-output "vpc_cidr_block" {
-  value = aws_vpc.vpc.cidr_block
-}
-
-output "main_route_table_id" {
-  value = aws_vpc.vpc.main_route_table_id
+output "vpc" {
+  value = aws_vpc.vpc
 }
 
 output "interface_vpce_sg_id" {
