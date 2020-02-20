f87ceef3d39e62642eafd41a4cf310d0cd319e96
Dmitri Kovalenko <39998089+tea-short@users.noreply.github.com>
2020-02-20 16:22:31 +0000

    Add EC2 Autoscaling VPC endpoint support (#24)

--- vpc_endpoints.tf
+++ vpc_endpoints.tf
@@ -174,6 +174,16 @@ resource "aws_vpc_endpoint" "elasticmapreduce" {
   private_dns_enabled = true
 }
 
+resource "aws_vpc_endpoint" "ec2autoscaling" {
+  count               = var.ec2autoscaling_endpoint ? 1 : 0
+  service_name        = "com.amazonaws.${var.region}.autoscaling"
+  vpc_id              = aws_vpc.vpc.id
+  vpc_endpoint_type   = "Interface"
+  security_group_ids  = [aws_security_group.vpc_endpoints.id]
+  subnet_ids          = var.interface_vpce_subnet_ids
+  private_dns_enabled = true
+}
+
 resource "aws_security_group" "vpc_endpoints" {
   name        = "vpc-endpoints-${var.vpc_name}"
   description = "Allows instances to reach Interface VPC endpoints"
