a8339ccf98aa9f0040241f52fb6d8326194293e1
MattToczek <52243130+MattToczek@users.noreply.github.com>
2020-04-22 17:09:47 +0100

    adding vpc endpoint for orchestration service to talk to load balancer (#26)
    * adding vpc endpoint for orchestration service to talk to load balancer
    
    * description amendment

--- variables.tf
+++ variables.tf
@@ -144,6 +144,11 @@ variable "ec2autoscaling_endpoint" {
   description = "Place an EC2 Auto Scaling VPC endpoint in this VPC. If set to true, you will also need to set `interface_vpce_source_security_group_ids` and `interface_vpce_subnet_ids`"
 }
 
+variable "elasticloadbalancing_endpoint" {
+  default     = false
+  description = "Place an elasticloadbalancing endpoint in this VPC. If set to true, you will also need to set `interface_vpce_source_security_group_ids` and `interface_vpce_subnet_ids`"
+}
+
 variable "gateway_vpce_route_table_ids" {
   default     = []
   description = "A list of one or more route table IDs for Gateway VPC Endpoint rules to be added to."
